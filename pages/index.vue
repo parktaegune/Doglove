41a3f5bcc81c3039c581b0d15d8aab5840300f71
parktaegune <qkrxo876@naver.com>
2018-05-15 10:11:39 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div class="contetn">
+      
+      
+    <h1 class="title is-1">개사랑</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
