88e596146a9286c35fe9c8094269beeea140f9a4
parktaegune <qkrxo876@naver.com>
2018-05-15 10:46:06 +0900

    (2018-05-08-4) init testjs

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
-  <section class="container">
-    <div class="hero is-primary">
-      <div class="hero-body">
-        <div="container">
-      
-    <h1 class="title is-1">개사랑</h1>
+  <section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Primary title
+      </h1>
+      <h2 class="subtitle">
+        Primary subtitle
+      </h2>
     </div>
-  </section>
+  </div>
+  <div class="columns is-desktop">
+  <div class="column">1</div>
+  <div class="column">2</div>
+  <div class="column">3</div>
+  <div class="column">4</div>
+</div>
+</section>
 </template>
 
 <script>
