8b7b558d183c4158e01389242bfeca60b2d21bb4
parktaegune <qkrxo876@naver.com>
2018-05-15 11:25:36 +0900

    (2018-05-08-4) init testjs

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
-        Primary title
+        개사랑
       </h1>
       <h2 class="subtitle">
-        Primary subtitle
+        개사진
       </h2>
     </div>
   </div>
@@ -16,10 +16,20 @@
   <div class="column">3</div>
   <div class="column">4</div>
 </div>
+
+<article>
+  
+
+<figure class="image is-128x128">
+  <img src="https://i.ytimg.com/vi/gvZKBwJwLjA/hqdefault.jpg">
+</figure>
+</article>
+
 </section>
 </template>
 
 <script>
+import axios from 'axios';
 
 </script>
 
